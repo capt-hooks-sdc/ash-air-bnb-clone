d061e1916a88065c59a6bad62d88aa3adda6c375
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-02-05 12:52:26 -0600

    completed seedfile generation script

--- hrr50-front-end-capstone-master/database/newDB/index.js
+++ hrr50-front-end-capstone-master/database/newDB/index.js
@@ -1,5 +1,13 @@
 const pgp = require('pg-promise')();
-const connectionString = 'postgres://localhost:5432/sdc';
+
+const PG_HOST = process.env.PG_HOST || 'localhost';
+const PG_PORT = process.env.PG_PORT || 5432;
+const PG_DB = process.env.PG_DB || 'sdc';
+
+const connectionString = `postgres://${PG_HOST}:${PG_PORT}/${PG_DB}`;
 const db = pgp(connectionString);
 
-module.exports = db;
+module.exports = {
+  db: db,
+  // Queries go here
+};

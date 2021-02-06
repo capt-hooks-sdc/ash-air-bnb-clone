838d4beed83c5929a0d84ece3d13b93c8c9aa64e
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-02-06 13:55:58 -0600

    added review counting. created alternate query function for benchmark comparison

--- hrr50-front-end-capstone-master/database/index.js
+++ hrr50-front-end-capstone-master/database/index.js
@@ -8,13 +8,36 @@ const connectionString = `postgres://${PG_HOST}:${PG_PORT}/${PG_DB}`;
 const db = pgp(connectionString);
 
 const retrieveOneProperty = (id, cb) => {
-  const queryString = `SELECT name, city, region, country, ph.url, ph.caption, r.stars FROM properties pr INNER JOIN photos ph ON ph.propertyId = pr.id INNER JOIN reviews r ON r.propertyId = pr.id WHERE pr.id = ${id}`;
+  const queryString = `SELECT pr.id, name, city, region, country, ph.url, ph.caption, SUM(r.stars) AS stars, COUNT(r.stars) AS reviews FROM properties pr LEFT JOIN photos ph ON ph.propertyId = pr.id LEFT JOIN reviews r ON r.propertyId = pr.id WHERE pr.id = ${id} GROUP BY pr.id, ph.id`;
   db.query(queryString)
-    .then(data => cb(null, data))
+    .then(data => {
+      var {id, name, city, region, country, stars, reviews} = data[0];
+      var property = {id, name, city, region, country, stars, reviews};
+      property.photos = data.map(photo => {
+        return {
+          url: photo.url,
+          caption: photo.caption
+        };
+      });
+      cb(null, property);
+    })
+    .catch(err => cb(err));
+};
+
+const retrievePropertySplit = (id, cb) => {
+  const prop = `SELECT p.id, name, city, region, country, SUM(r.stars) AS stars, COUNT(r.stars) AS reviews FROM properties p LEFT JOIN reviews r ON r.propertyId = p.id WHERE p.id = ${id} GROUP BY p.id`;
+  const photos = `SELECT url, caption FROM photos WHERE propertyId = ${id}`;
+  Promise.all([db.one(prop), db.query(photos)])
+    .then(data => {
+      var property = data[0];
+      property.photos = data[1];
+      cb(null, property);
+    })
     .catch(err => cb(err));
 };
 
 module.exports = {
-  db: db,
-  retrieveOneProperty: retrieveOneProperty
+  db,
+  retrieveOneProperty,
+  retrievePropertySplit
 };

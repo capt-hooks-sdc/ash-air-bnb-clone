31bea2f6f450a525208cbb98ae1c8364a20178bf
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-02-06 13:56:59 -0600

    updated ID to query for to be penultimate in DB

--- hrr50-front-end-capstone-master/database/database.test.js
+++ hrr50-front-end-capstone-master/database/database.test.js
@@ -1,7 +1,7 @@
 const {retrieveOneProperty} = require('./index.js');
 
 console.time('A');
-retrieveOneProperty(9000000, (err, results) => {
+retrieveOneProperty(9999999, (err, results) => {
   console.timeEnd('A');
   if (err) {
     console.log('ENDED WITH ERROR: ', err);

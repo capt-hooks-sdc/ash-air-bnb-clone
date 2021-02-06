a88f0983b3722b669f338cb0d59d0aeba967343c
Garrett Rubinstein <garrub.bergen@gmail.com>
2021-02-06 14:44:03 -0600

    added separate tests for grouped/split query timing

--- hrr50-front-end-capstone-master/database/database.test.js
+++
@@ -1,9 +0,0 @@
-const {retrieveOneProperty} = require('./index.js');
-
-console.time('A');
-retrieveOneProperty(9999999, (err, results) => {
-  console.timeEnd('A');
-  if (err) {
-    console.log('ENDED WITH ERROR: ', err);
-  }
-});
